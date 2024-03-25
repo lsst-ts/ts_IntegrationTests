b4fdf36faea9427cf92417a48752615a6d4818bd
Rob Bovill <rbovill@lsst.org>
2024-03-25 15:29:16 -0700

    Created the separate auxtel_latiss_acquire.py and auxtel_latiss_take_sequence.py scripts to handle the two new telescope scripts. Added the appropriate configs for each. These now run the nominal, verify and test configurations.
    Added the two new scripts to conda/meta.yaml and pyproject.toml.

---
+++ python/lsst/ts/IntegrationTests/auxtel_latiss_take_sequence.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# This file is part of ts_IntegrationTests.
+#
+# Developed for the Vera C. Rubin Observatory Telescope & Site Software system.
+# This product includes software developed by the Vera C. Rubin Observatory
+# Project (https://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+
+__all__ = [
+    "AuxTelLatissTakeSequence",
+    "run_auxtel_latiss_take_sequence",
+]
+
+import argparse
+import asyncio
+
+from lsst.ts.IntegrationTests import BaseScript
+
+from .configs.config_registry import registry
+
+
+class AuxTelLatissTakeSequence(BaseScript):
+    """Execute the given Standard or External script,
+    with the given Yaml configuration,
+    placed in the given ScriptQueue location.
+
+    Parameters
+    ----------
+    sequence : `str`
+        Defines which sequence to run.
+        Choices are ["verify", "nominal", "test"].
+    """
+
+    index: int = 2
+    configs: tuple = ([],)
+    scripts: list = [
+        ("auxtel/latiss_take_sequence.py", BaseScript.is_external),
+    ]
+
+    def __init__(self, sequence: str) -> None:
+        super().__init__()
+        self.sequence = sequence
+        self.configs = (registry[f"auxtel_take_sequence_{sequence}"],)
+
+
+def run_auxtel_latiss_take_sequence() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "sequence",
+        type=str,
+        choices=["verify", "nominal", "test"],
+        help="Specify which sequence to run.",
+    )
+    args = parser.parse_args()
+    script_class = AuxTelLatissTakeSequence(sequence=args.sequence)
+    print(
+        f"\nAuxTel Latiss Take Sequence; "
+        f"running the {script_class.scripts[0][0]} script, "
+        f"for the {script_class.sequence} sequence, "
+        f"with configuration;\n{script_class.configs}"
+    )
+    asyncio.run(script_class.run())
