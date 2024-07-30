362c0512ccb171a518eb03c0fc4f4fa6fe136f29
Rob Bovill <rbovill@lsst.org>
2024-07-30 15:41:28 -0700

    Re-added MTPtg to love_stress_test_configs.py.

--- python/lsst/ts/IntegrationTests/configs/love_stress_test_configs.py
+++ python/lsst/ts/IntegrationTests/configs/love_stress_test_configs.py
@@ -48,6 +48,7 @@ registry["love_stress"] = yaml.safe_dump(
             "MTAirCompressor:1",
             "MTAirCompressor:2",
             "MTMount:0",
+            "MTPtg:0",
             "MTDome:0",
             "MTDomeTrajectory:0",
             "MTAOS:0",
