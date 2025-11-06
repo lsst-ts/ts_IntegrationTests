8f5a6c12ca00437b8453159e70ce29c517c315fb
rbovill <rbovill@gmail.com>
2025-11-05 17:00:17 -0700

    Add maintel_move_rotator.py integration test script. Add unit tests.

--- python/lsst/ts/IntegrationTests/__init__.py
+++ python/lsst/ts/IntegrationTests/__init__.py
@@ -65,6 +65,7 @@ from .love_stress_test import *
 from .lsstcam_calibrations import *
 from .maintel_disabled_enabled import *
 from .maintel_housekeeping import *
+from .maintel_move_rotator import *
 from .maintel_open_mirror_covers import *
 from .maintel_point_azel import *
 from .maintel_slew_dome import *
