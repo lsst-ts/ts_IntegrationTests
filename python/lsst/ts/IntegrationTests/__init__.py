7d0cce29803ede6af00c9d293a38e483e479379a
rbovill <rbovill@gmail.com>
2026-05-20 11:23:03 -0700

    Add raise_m1m3 and enable_m1m3_slew_controller_flags scripts.

--- python/lsst/ts/IntegrationTests/__init__.py
+++ python/lsst/ts/IntegrationTests/__init__.py
@@ -64,11 +64,13 @@ from .love_stress_test import *
 from .lsstcam_calibrations import *
 from .maintel_csc_end_of_night import *
 from .maintel_disabled_enabled import *
+from .maintel_enable_m1m3_slew_controller_flags import *
 from .maintel_housekeeping import *
 from .maintel_lower_m1m3 import *
 from .maintel_move_rotator import *
 from .maintel_open_mirror_covers import *
 from .maintel_point_azel import *
+from .maintel_raise_m1m3 import *
 from .maintel_slew_dome import *
 from .maintel_standby_disabled import *
 from .obssys_disabled_enabled import *
